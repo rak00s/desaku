d5da57b0db1c498803055072d6756b9609a26e64
Anindya Sita Paramastri <128381574+rak00s@users.noreply.github.com>
2024-12-14 18:48:21 +0700

    Delete application directory

--- application/logs/log-2024-12-13.php
+++
@@ -1,9 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-
-ERROR - 2024-12-13 15:43:36 --> Severity: Warning --> Undefined array key 0 C:\xampp\htdocs\desaku\modules\dashboard\views\index.php 50
-ERROR - 2024-12-13 15:43:36 --> Severity: Warning --> Undefined array key 1 C:\xampp\htdocs\desaku\modules\dashboard\views\index.php 50
-ERROR - 2024-12-13 15:43:37 --> 404 Page Not Found: /index
-ERROR - 2024-12-13 15:43:37 --> 404 Page Not Found: /index
-ERROR - 2024-12-13 15:43:37 --> 404 Page Not Found: /index
-ERROR - 2024-12-13 15:43:37 --> 404 Page Not Found: /index
-ERROR - 2024-12-13 15:43:37 --> 404 Page Not Found: /index
